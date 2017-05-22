1eb0b30c5dd3fe55a38822977679bcba96619fda
Devon Hogencamp <devhogen1@gmail.com>
2017-05-22 14:14:19 -0700

    Built ability to get and create todos from fronted

--- public/core.js
+++ public/core.js
@@ -13,5 +13,37 @@ var TodoApp = angular.module('TodoApp', []);
 
 // Main Controller - Handles all operations of Todo App
 function mainController($scope, $http) {
+
+    $scope.formData = {};
+
+    // When we first get to index.html we will instantly get all of the todos
+    $http.get('api/todos')
+        .success(function (data) {
+            $scope.todos = data;
+
+            console.log(data);
+        })
+        .error(function (err) {
+            console.log('Error: ' + err);
+        });
+
+    // When we click submit on the add form send the text to the Create part of our API
+    $scope.createTodo = function () {
+        $http.post('api/todos', $scope.formData)
+            .success(function (data) {
+
+                // Clear the form data so the user can enter another item in
+                $scope.formData = {};
+
+                $scope.todos = data;
+
+                console.log(data);
+
+            })
+            .error(function (err) {
+                console.log('Error: ' + err);
+            });
+    };
+
     
 }
