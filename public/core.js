eed06564547964402c67f96742ce1600b0eea47f
Devon Hogencamp <devhogen1@gmail.com>
2017-05-22 13:56:39 -0700

    Start of our core.js file that handles
    This file will handle all of the angular for our app. Such as handling
    the todos and applying new views.

---
+++ public/core.js
@@ -0,0 +1,17 @@
+/*
+* core.js
+* ====================
+* Our main Angluar file
+* Module
+* Controller
+* Functions to handle todos
+* Apply to view
+*/
+
+// Setup this main module
+var TodoApp = angular.module('TodoApp', []);
+
+// Main Controller - Handles all operations of Todo App
+function mainController($scope, $http) {
+    
+}
