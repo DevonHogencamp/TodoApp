0a8f910c70d45089f97b186e255dd692d1a18cb0
Devon Hogencamp <devhogen1@gmail.com>
2017-05-22 13:19:50 -0700

    Our Todo Model
    We use this so that the server can communicate with MongoDB it lays out
    a model for all todos

--- server.js
+++ server.js
@@ -55,6 +55,13 @@ app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 app.use(methodOverride());
 
+/*
+* ToDo Model
+*/
+var Todo = mongoose.model('Todo', {
+    text: String
+});
+
 // listen (start app with node server.js)
 app.listen(8080);
 console.log("App listening on port 8080");
